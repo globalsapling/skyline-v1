df63cfec2a6b4d2522b80b2e4c6ea105309cd83f
globalsapling1 <30220712-globalsapling1@users.noreply.replit.com>
2023-12-09 22:39:01 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,187 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import './App.css';
+import abi from './utils/WavePortal.json';
+import coverImage from '../assets/IMG_7234.jpg';
+
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [allWaves, setAllWaves] = useState([]);
+
+  /**
+   * Create a varaible here that holds the contract address after you deploy!
+   */
+  const contractAddress = "0xe63D18B09091f784d67b1A18d359117565464e19";
+  const contractABI = abi.abi;
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (currentAccount) {
+        getAllWaves();
+      }
+      
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        const waveTxn = await wavePortalContract.wave("Rameshwaram Cafe to KTPO");
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+        getAllWaves();
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        /*
+         * Call the getAllWaves method from your Smart Contract
+         */
+        const waves = await wavePortalContract.getAllWaves();
+
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        /*
+         * Store our data in React State
+         */
+        setAllWaves(wavesCleaned);
+
+        wavePortalContract.on("NewWave", (from, timestamp, message) => {
+          console.log("NewWave", from, timestamp, message);
+
+          setAllWaves(prevState => [...prevState, {
+            address: from,
+            timestamp: new Date(timestamp * 1000),
+            message: message
+          }]);
+        });
+        
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  console.log('All Waves:', allWaves);
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <img src={coverImage} alt="Cover" className="coverImage" />
+        <div className="header">
+        🧭 Welcome to Skyline! 
+        </div>
+
+        <div className="bio">
+          This is an experimental automated credentialing and flightplanning system for your beyond line-of-sight commercial drone flights. 
+        </div>
+
+  
+        <button className="waveButton" onClick={wave}>
+          Authenticate Pilot Credential and File Flightplan
+        </button>
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet (if you are a first time user)
+          </button>
+        )}
+        <div className="bio">
+          List of recently filed flightplans: 
+        </div>
+    
+        {allWaves.reverse().map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "oldlace", marginTop: "16px", padding: " 15px" }}>
+              <div>Pilot Credential: {wave.address}</div>
+              <div>Departure Time: {wave.timestamp.toString()}</div>
+            </div>)
+    })}
+        
+      </div>
+    </div>
+  );
+}
+
+export default App
